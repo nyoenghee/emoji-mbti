1f935a9d797f5a81eca6367bf5c7462a01fe5282
ji-minseo <jijiminseo@gmail.com>
2023-10-06 14:52:38 +0900

    add basics

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import '../src/scss/Style.scss'
 function App() {
 	return (
 		<div className="section section-main">
-			<div className="section-title">emoji mbti ✨</div>
-
-			<button className="section-btn">start</button>
+			<div className="section-title f-h1">emoji mbti ✨</div>
+			<span className='section-desc f-p'>find your own emoji</span>
+			<button className="section-btn f-subtitle">start</button>
 		</div>
 	);
 }
