51dcb088cd16920208f0bcf8c384bbb552d2987f
ji-minseo <jijiminseo@gmail.com>
2023-10-06 15:07:38 +0900

    add main img

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import '../src/scss/Style.scss'
 function App() {
 	return (
 		<div className="section section-main">
-			<div className="section-title f-h1">emoji mbti ✨</div>
-			<span className='section-desc f-p'>find your own emoji</span>
+			<div className="section-title f-h1">emoji mbti</div>
+			<span className='section-desc f-title'>find your own emoji</span>
 			<button className="section-btn f-subtitle">start</button>
 		</div>
 	);
