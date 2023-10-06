0ccce86fb602c581e63b50a3c511b51d597cef4c
ji-minseo <jijiminseo@gmail.com>
2023-10-06 14:21:22 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className="main-section">
+			<div className="title">emoji mbti ✨</div>
+
+			<button className="btn">start</button>
+		</div>
+	);
+}
+
+export default App;
