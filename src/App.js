d07daa83cfdbafac718cdd7de413efc7b2bc0291
ji-minseo <jijiminseo@gmail.com>
2023-10-06 15:23:15 +0900

    add credit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
 		<div className="section section-main">
 			<div className="section-title f-h1">emoji mbti</div>
 			<span className='section-desc f-title'>find your own emoji</span>
+			<span className='section-credit f-p'>서지민, 홍녕희의 리액트로 토이프로젝트 만들기</span>
 			<button className="section-btn f-subtitle">start</button>
 		</div>
 	);
