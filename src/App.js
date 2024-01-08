0e2d86d2d96e7e20c989b3bc580b1977ec6faedb
ji-minseo <105339059+does-jimin@users.noreply.github.com>
2024-01-08 18:07:07 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import '../src/scss/Style.scss'
 function App() {
 	return (
 		<div className="section section-main">
-			<div className="section-title f-h1">스티커팩 성격 테스트</div>
-			<span className='section-desc f-title'>스티커팩으로 만들어지는 내 성격유형</span>
-			<span className='section-credit f-p'>하하</span>
+			<div className="section-title f-h1">이모지 스티커팩 성격 테스트</div>
+			<span className='section-desc f-title'>이모지 스티커팩으로 만들어지는 내 성격유형</span>
+			<span className='section-credit f-p'>나의 성격 유형에 맞는 이모지 스티커팩은 무엇일까?</span>
 			<button className="section-btn f-subtitle">start</button>
 		</div>
 	);
