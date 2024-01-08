4aa88c28e57ca6159499f57986d38d8b5781dce3
ji-minseo <105339059+does-jimin@users.noreply.github.com>
2024-01-08 18:04:21 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import '../src/scss/Style.scss'
 function App() {
 	return (
 		<div className="section section-main">
-			<div className="section-title f-h1">emoji mbti</div>
-			<span className='section-desc f-title'>find your own emoji</span>
-			<span className='section-credit f-p'>서지민, 홍녕희의 리액트로 토이프로젝트 만들기</span>
+			<div className="section-title f-h1">스티커팩 성격 테스트</div>
+			<span className='section-desc f-title'>스티커팩으로 만들어지는 내 성격유형</span>
+			<span className='section-credit f-p'>하하</span>
 			<button className="section-btn f-subtitle">start</button>
 		</div>
 	);
