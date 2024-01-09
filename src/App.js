1d60679e3b5de6e0fb58b69b063c598abfefa840
ji-minseo <105339059+does-jimin@users.noreply.github.com>
2024-01-09 11:52:57 +0900

    fixinf

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import '../src/scss/Style.scss'
 function App() {
 	return (
 		<div className="section section-main">
-			<div className='card'>
-				<div className="section-title f-h1">이모지 스티커팩 성격 테스트</div>
-				<span className='section-desc f-title'>이모지 스티커팩으로 만들어지는 내 성격유형</span>
-				<span className='section-credit f-p'>나의 성격 유형에 맞는 이모지 스티커팩은 무엇일까?</span>
-				<button className="section-btn f-subtitle">start</button>
+			<div className='header'>
+
+				<div className='inner'>
+					<div className='__left'>이모지 스티커팩 성격 테스트</div>
+					<div className='__right'>나의 성격 유형에 맞는 이모지 스티커팩은 무엇일까?</div>
+				</div>
 			</div>
+			<div className='emojis'>
+					<img src='https://d2pas86kykpvmq.cloudfront.net/img_emoji/2.0/Preview-19.png'/>
+					{/* <img src='https://d2pas86kykpvmq.cloudfront.net/img_emoji/2.0/Preview-13.png'/>
+					<img src='https://d2pas86kykpvmq.cloudfront.net/img_emoji/2.0/Preview-34.png'/>
+					<img src='https://d2pas86kykpvmq.cloudfront.net/img_emoji/2.0/Preview-6.png'/>
+					<img src='https://d2pas86kykpvmq.cloudfront.net/img_emoji/2.0/Preview-11.png'/>
+					<img src='https://d2pas86kykpvmq.cloudfront.net/img_emoji/2.0/Preview-26.png'/>
+					<img src='https://d2pas86kykpvmq.cloudfront.net/img_emoji/2.0/Preview-5.png'/> */}
+					
+				</div>
+			<div className='btn'>✨시작하기 ➧</div>
 		</div>
 	);
 }
